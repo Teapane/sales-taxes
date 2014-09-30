36ea5711561e2e6f76067e86807f894656271575
Brad Scharmann <bscharm@gmail.com>
2014-09-29 18:13:30 -0600

    removed arguments in line item builder

--- lib/line_item_builder.rb
+++ lib/line_item_builder.rb
@@ -8,7 +8,8 @@ class LineItemBuilder
 
   def return_formatted_data
     File.open(@file).each_with_object([]) do |line, array|
-      array << build_line_item(line)
+      @line = line
+      array << build_line_item
     end
   end
 
@@ -16,55 +17,56 @@ class LineItemBuilder
 
   attr_reader :file
 
-  def build_line_item(line)
+  def build_line_item
     LineItem.new({
-                   description: return_item_description(line),
-                   quantity: return_quantity(line),
-                   price: return_price(line),
-                   imported: return_imported(line),
-                   type: return_type(line)
+
+                   description: return_item_description,
+                   quantity: return_quantity,
+                   price: return_price,
+                   imported: return_imported,
+                   type: return_type
                  })
   end
 
-  def return_quantity(line)
-    /^\d+/.match(line).to_s.to_i
+  def return_quantity
+    /^\d+/.match(@line).to_s.to_i
   end
 
-  def return_price(line)
-    /(?<=at\s)\d+.\d+/.match(line).to_s.to_f
+  def return_price
+    /(?<=at\s)\d+.\d+/.match(@line).to_s.to_f
   end
 
-  def return_item_description(line)
-    /(?<=\d\s).*(?=\sat)/.match(line).to_s
+  def return_item_description
+    /(?<=\d\s).*(?=\sat)/.match(@line).to_s
   end
 
-  def return_imported(line)
-    return_item_description(line).include?('imported')
+  def return_imported
+    return_item_description.include?('imported')
   end
 
-  def return_type(line)
-    description = return_item_description(line)
-    if description_indicates_food(description)
+  def return_type
+    @description = return_item_description
+    if description_indicates_food
       :food
-    elsif description_indicates_medicine(description)
+    elsif description_indicates_medicine
       :medicine
-    elsif description_indicates_book(description)
+    elsif description_indicates_book
       :book
     else
       :other
     end
   end
 
-  def description_indicates_food(description)
-    description.include?('chocolate')
+  def description_indicates_food
+    @description.include?('chocolate')
   end
 
-  def description_indicates_medicine(description)
-    description.include?('pills')
+  def description_indicates_medicine
+    @description.include?('pills')
   end
 
-  def description_indicates_book(description)
-    description.include?('book')
+  def description_indicates_book
+    @description.include?('book')
   end
 
 end
