0f84049bea38dadfe4da523891c374e81bbae0f3
Brad Scharmann <bscharm@gmail.com>
2014-09-29 18:34:58 -0600

    removed argument from rounding method in tax calculator

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -13,12 +13,12 @@ class TaxCalculator
   attr_reader :line_item
 
   def calculate_sales_tax
-    tax = (line_item.price * line_item.tax_rate) / 100.0
-    round_to_nearest_05(tax)
+    @tax = (line_item.price * line_item.tax_rate) / 100.0
+    round_to_nearest_05
   end
 
-  def round_to_nearest_05(value)
-    ((value * 20.0).ceil / 20.0)
+  def round_to_nearest_05
+    ((@tax * 20.0).ceil / 20.0)
   end
 
 
